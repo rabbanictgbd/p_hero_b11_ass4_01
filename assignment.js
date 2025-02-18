75aceaefb60227901c11723856ec4c8ba7739e42
Abdul Kader Rabbani <rabbanictgbd@gmail.com>
2025-02-18 16:46:29 +0600

    assignment 4 start

---
+++ assignment.js
@@ -0,0 +1,21 @@
+function cashOut( money ) {
+    return money/100*1.75;
+}
+
+console.log(cashOut(1000))
+
+function  validEmail( email ) {
+    // You have to write your code here
+}
+
+function  electionResult( votes ) {
+    // You have to write your code here
+}
+
+function  isBestFriend( f1 , f2 ) {
+    // You have to write your code here
+}
+
+function  calculateWatchTime( times ) {
+    // You have to write your code here
+}
