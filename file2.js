4b678f1526e3cfad4fe49a7770193a2141ef76cf
Abdul Kader Rabbani <rabbanictgbd@gmail.com>
2025-02-20 17:21:19 +0600

    vote

--- file2.js
+++ file2.js
@@ -30,23 +30,46 @@ console.log("email", validEmail("rabbanictgbdgmail"))
 // console.log("email",validEmail(rabbanictgbdgmail))
 // console.log("email",validEmail(152))
 
-const items = ["mango", "banana", "naVote"];
+// const items = ["mango", "banana", "naVote"];
 function electionResult(votes) {
-    const items = ["mango", "banana", "naVote"];
+    
+    let mangoCounter=0;
+    let bananaCounter=0;
     if (Array.isArray(votes)) {
-        const mango = votes[0];
-        const banana = votes[1];
-        const naVote = votes[2];
-        return votes
-    } return "invalid"
 
-}
+        for(const vote of votes){
+            
+            // console.log(vote)
+           if (vote == "mango"){
+            mangoCounter++
+           }
+           if (vote == "banana"){
+            bananaCounter++
+           }
+        }
+        if (mangoCounter>bananaCounter){
+            
+            return  [mangoCounter, "Mango"]
+        } else if (bananaCounter>mangoCounter){
+            return [bananaCounter, "Manana"]
+        } return [bananaCounter, mangoCounter, "Drew"]
+    }
 
-// console.log(electionResult(votes.indexOf("banana")));
+    
+    return "invalid" 
+        
+    }
 
 
-console.log(electionResult(10));
-console.log(electionResult(items));
+
+
+
+
+// console.log(electionResult(10));
+// console.log(["mango", "banana", "naVote"]);
+console.log("...........................")
+console.log(electionResult(["mango","mango", "banana","banana", "naVote"]));
+// console.log(vote);
 
 function isBestFriend(f1, f2) {
     // You have to write your code here
@@ -56,17 +79,17 @@ function calculateWatchTime(times) {
     // You have to write your code here
 }
 
-function typeTest(type) {
-    if (typeof type === "string") {
-        return "string";
-    } else if (typeof type === 1.4) {
-        return "number";
-    } else {
-        return "others";
-    }
+// function typeTest(type) {
+//     if (typeof type === "string") {
+//         return "string";
+//     } else if (typeof type === 1.4) {
+//         return "number";
+//     } else {
+//         return "others";
+//     }
 
-}
+// }
 
 
-console.log(typeTest("hello"));
-console.log(typeTest(100));
+// console.log(typeTest("hello"));
+// console.log(typeTest(100));
